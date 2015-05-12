97e78f202a66ae3cc8bf0dfa23004c6f9fa72125
Pedro Almeida <pedro@viur.pt>
2015-05-12 14:18:56 +0100

    config update

---
+++ schema.sql
@@ -0,0 +1,23 @@
+CREATE TABLE def_servpath_urn_smartsantander_testbed_357_santander_soundacc
+(
+  recvTimeTs LONGTEXT,
+  recvTime   TIMESTAMP,
+  entityId   LONGTEXT,
+  entityType LONGTEXT,
+  attrName   VARCHAR(255),
+  attrType   VARCHAR(255),
+  attrValue  DOUBLE,
+  attrMd     LONGTEXT
+);
+
+CREATE TABLE def_servpath_meteolo_meteolo
+(
+  recvTimeTs LONGTEXT,
+  recvTime   TIMESTAMP,
+  entityId   LONGTEXT,
+  entityType LONGTEXT,
+  attrName   VARCHAR(255),
+  attrType   VARCHAR(255),
+  attrValue  DOUBLE,
+  attrMd     LONGTEXT
+);
